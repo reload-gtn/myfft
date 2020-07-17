a74ca07de667e5fd7f1cb11aadc8f1f4d9dd4126
reload-gtn <reload-gtn@mail.ru>
2020-07-17 16:07:55 +0300

    first

--- src/MyFFT.java
+++ src/MyFFT.java
@@ -1,47 +1,80 @@
 public class MyFFT {
+    //число точек дискретного преобразования
+    static int Ndft = 100;
+    //частота дискретизации
+    static double Fdft = 8000;
+
+    //расчет дискретного преобразования Фурье
+    public Complex[] dft(double[] data){
+        Complex[] var_dft = new Complex[Ndft];
+        for (int i = 0; i < Ndft; i++) {
+            Complex dft = new Complex(0,0);
+            for (int j = 0; j < Ndft; j++) {
+                dft.add(new Complex (data[j]*Math.cos(2*Math.PI*j*i/(double)Ndft), -data[j]*Math.sin(2*Math.PI*j*i/(double)Ndft)));
+            }
+            var_dft[i] = dft;
+        }
+        return var_dft;
+    }
+/*
+    private static Complex w(double k, double N)
+    {
+        if (k % N == 0) return new Complex(1, 1);
+        double arg = -2 * Math.PI * k / N;
+        return new Complex(Math.cos(arg), Math.sin(arg));
+    }
+
+    public static Complex[] fft(Complex[] x)
+    {
+        Complex[] X;
+        int N = x.length;
+        if (N == 2)
+        {
+            X = new Complex[2];
+            x[0].add(x[1]);
+            X[0] =x[0];
+            x[0].sub(x[0]);
+            X[1] = x[0];
+        }
+        else
+        {
+            Complex[] x_even = new Complex[N / 2];
+            Complex[] x_odd = new Complex[N / 2];
+            for (int i = 0; i < N / 2; i++)
+            {
+                x_even[i] = x[2 * i];
+                x_odd[i] = x[2 * i + 1];
+            }
+            Complex[] X_even = fft(x_even);
+            Complex[] X_odd = fft(x_odd);
+            X = new Complex[N];
+            for (int i = 0; i < N / 2; i++)
+            {
+                X[i] = X_even[i] + w(i, N) * X_odd[i];
+                X[i + N / 2] = X_even[i] - w(i, N) * X_odd[i];
+            }
+        }
+        return X;
+    }
+*/
     public static void main (String[] args) {
         //амплитуда сигнала
-        double A = 2;
+        double A = 1;
         //частота сигнала
-        double f = 32;
-        double fd;
-        //частота дискретизации
-        int Fs = 500;
-        //число отсчетов
-        int Nf = 10;
-        double[] var = new double[Nf];
-        Complex[] var_fft = new Complex[Nf];
+        double f = 400;
+        //шаг по частоте
+        double fd = Fdft/Ndft;
+        //массив под данные
+        double[] var = new double[Ndft];
 
-        for (int i = 0; i < Nf; i++) {
-            var[i] = 1.0;
-            //var[i]=A*Math.cos(2*Math.PI*f*i/(double)Nf);
-        }
-        //System.out.println(var[1]);
-
-        for (int i = 0; i < Nf; i++) {
-            Complex fft = new Complex(0,0);
-            for (int j = 0; j < Nf; j++) {
-                //fft.add(new Complex (1, -1));
-                fft.add(new Complex (var[j]*Math.cos(2*Math.PI*j*i/(double)Nf), -var[j]*Math.sin(2*Math.PI*j*i/(double)Nf)));
-            }
-            var_fft[i] = fft;
-            //System.out.println(var_fft[i].toString());
+        for (int i = 0; i < Ndft; i++) {
+            var[i]=A*Math.cos(2*Math.PI*f*i/Fdft);
         }
 
-        for (int i = 0; i < Nf; i++) {
-            fd = (i*Fs)/(double) Nf;
-            //System.out.println(fd);
-            System.out.println((double) fd + " Гц: " + var_fft[i].toString());
+        MyFFT test = new MyFFT();
+
+        for (int i = 0; i <= test.dft(var).length/2; i++) {
+            System.out.println(i*fd+ " Гц: " + 2*test.dft(var)[i].abs()/Ndft);
         }
-
-        //Complex a = new Complex(1,1);
-        //Complex b = new Complex(1,-1);
-        //System.out.println(a.toString());
-        //System.out.println(b.toString());
-        //Complex c = a;
-        //a.add(b);
-        //System.out.println(a.toString());
-        //System.out.println(a.getRe() + " + " + a.getIm() + "i");
-
     }
 }
